d5d355849b54f9a18205d32c908cbb0e7f59e88d
Sairaj <srkhandagale1601@gmail.com>
2026-07-05 19:03:58 +0530

    feat: add logger middleware for request duration tracking

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import urlRoutes from "./modules/url/url.routes";
 import { errorHandler } from "./middleware/errorHandler";
+import { logger } from "./middleware/logger";
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 app.use("/urls",urlRoutes);
+app.use("/api/url",urlRoutes);
 app.use(errorHandler);
 export default app;
